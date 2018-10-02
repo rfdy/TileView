014a33be85347b41fb9776ad3cca325486244efd
Joel Kronenberg <joel.kronenberg@yp.ca>
2018-10-02 09:14:30 -0400

    updated to androidx

--- src/com/qozix/tileview/tiles/TileCache.java
+++ src/com/qozix/tileview/tiles/TileCache.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
-import android.support.v4.util.LruCache;
+import androidx.collection.LruCache;
 
 
 import com.jakewharton.disklrucache.DiskLruCache;
